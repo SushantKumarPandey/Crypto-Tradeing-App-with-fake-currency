cc325a1ca37c9f235d8b9cc7359ae5ecb581d506
victo <victoria.S03@outlook.como>
2025-05-06 14:51:38 +0200

    feat: DB und User-Table

---
+++ server/db.py
@@ -0,0 +1,22 @@
+import sqlite3
+from datetime import datetime
+
+from flask import g, current_app
+
+
+conn = sqlite3.connect('crypto.db')
+c = conn.cursor()
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+        current_app.cnofig['DATABASE'],
+        detect_types = sqlite3.PARSE_DECLTYPES)
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
