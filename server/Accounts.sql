cbf915a4255c17f56a37b97232ea0eef49543bb1
victo <victoria.S03@outlook.como>
2025-05-12 10:12:34 +0200

    fix: DB, Account

--- server/Accounts.sql
+++ server/Accounts.sql
@@ -3,7 +3,9 @@ DROP TABLE IF EXISTS user;
 CREATE TABLE user(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    email TEXT NOT NULL
 );
 
-INSERT INTO user VALUES (1,'kiki','quack')
+INSERT INTO user (username, password, email)
+    VALUES ('kiki','quack', 'bsp@gmail.com');
