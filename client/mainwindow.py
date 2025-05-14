6a7ef4bf3e05b43481f1947aacc94f0183584b9c
victo <victoria.S03@outlook.como>
2025-05-14 09:02:17 +0200

    feat: login

--- client/mainwindow.py
+++ client/mainwindow.py
@@ -1,11 +1,8 @@
 import sys
-import unittest
-import sqlite3
-from PyQt6 import QtWidgets, uic
-from client import *
-from werkzeug.security import generate_password_hash
+from dbm import sqlite3
 
-from client.Crypto import parameters
+from PyQt6 import QtWidgets, uic
+from werkzeug.security import check_password_hash
 
 
 class Registerwindow(QtWidgets.QDialog):
@@ -13,55 +10,35 @@ class Registerwindow(QtWidgets.QDialog):
         super().__init__()
         uic.loadUi("register.ui", self)
 
-        self.pushButton_register.clicked.connect(self.register)
-
-    def register(self):
-
-        username = self.lineEdit_username.text()
-        password = self.lineEdit_password.text()
-        email = self.lineEdit_email.text()
-
-        hashed_password = generate_password_hash(password)
-
-        if username == '' or password == '' or email == '':
-            QtWidgets.QMessageBox.warning(self, "Error", "Please fill all fields")
-            return
-
-        try:
-            print('connecting ...')
-            conn = sqlite3.connect('crypto.db')
-            c = conn.cursor()
-            print('Executing Insert')
-
-            c.execute('''
-                    INSERT INTO user (username, password, email) 
-                    VALUES (?,?,?)
-                ''', (username, hashed_password, email))
-            conn.commit()
-            print('Insert done')
-            QtWidgets.QMessageBox.information(self, 'Account created',
-                                              "Your account has been created! You are now able to log in.")
-            self.close()
-
-        except sqlite3.Error as e:
-            print(f"SQLite Error: {e}")
-            QtWidgets.QMessageBox.warning(self, "Error", f"SQLite Error: {e}")
-
-        finally:
-            conn.close()
-
 
 class Loginwindow(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi("login.ui", self)
 
-        self.Register.clicked.connect(self.show_login)
-        self.register_window = None
+        self.pushButton_to_login.clicked.connect(self.verify_login)
 
-    def show_login(self):
-        self.register_window = Registerwindow()
-        self.register_window.show()
+    def verify_login(self):
+
+        username = self.lineEdit_password_2.text()
+        password = self.lineEdit_password.text()
+
+        try:
+            conn = sqlite3.connect("crypto.db")
+            c = conn.cursor()
+            c.execute("SELECT id, password FROM users WHERE username = ?", (username,))
+            user = c.fetchone()
+            conn.close()
+
+            if user and check_password_hash(user[1], password):
+                QtWidgets.QMessageBox.information(self, "Login Success", "Successfully Logged In")
+                self.accept()
+            else:
+                QtWidgets.QMessageBox.warning(self, "Login Failed", "Invalid Username or Password")
+        except Exception as e:
+            QtWidgets.QMessageBox.critical(self, "Error", f"An error occurred:\n{e}")
+
+
 
 class Mainwindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -69,28 +46,15 @@ class Mainwindow(QtWidgets.QMainWindow):
         uic.loadUi("form.ui", self)
 
         self.loginButton.clicked.connect(self.login_show)
-        self.Accounts_2.itemClicked.connect(self.fetch_table())
         self.login_window = None
 
     def login_show(self):
         self.login_window = Loginwindow()
         self.login_window.show()
 
-    def fetch_table(self):
-        url = ("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest")
-        parameters = {
-            'symbol'
-        }
-        headers
-        try:
-            response = session.get(url, params=parameters)
-            data = json.loads(response.text)
-            print(data)
-        except (ConnectionError, Timeout, TooManyRedirects) as e:
-            print(e)
-
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = Mainwindow()
     window.show()
     sys.exit(app.exec())
+
