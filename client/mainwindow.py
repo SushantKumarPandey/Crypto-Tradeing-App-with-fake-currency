08f28200f54a995485971f0a66213bad78073420
victo <victoria.S03@outlook.como>
2025-05-14 09:02:17 +0200

    feat: login

--- client/mainwindow.py
+++ client/mainwindow.py
@@ -1,22 +1,44 @@
 import sys
+from dbm import sqlite3
+
 from PyQt6 import QtWidgets, uic
+from werkzeug.security import check_password_hash
+
 
 class Registerwindow(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi("register.ui", self)
 
+
 class Loginwindow(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi("login.ui", self)
 
-        self.Register.clicked.connect(self.show_login)
-        self.register_window = None
+        self.pushButton_to_login.clicked.connect(self.verify_login)
 
-    def show_login(self):
-        self.register_window = Registerwindow()
-        self.register_window.show()
+    def verify_login(self):
+
+        username = self.lineEdit_password_2.text()
+        password = self.lineEdit_password.text()
+
+        try:
+            conn = sqlite3.connect("crypto.db")
+            c = conn.cursor()
+            c.execute("SELECT id, password FROM users WHERE username = ?", (username,))
+            user = c.fetchone()
+            conn.close()
+
+            if user and check_password_hash(user[1], password):
+                QtWidgets.QMessageBox.information(self, "Login Success", "Successfully Logged In")
+                self.accept()
+            else:
+                QtWidgets.QMessageBox.warning(self, "Login Failed", "Invalid Username or Password")
+        except Exception as e:
+            QtWidgets.QMessageBox.critical(self, "Error", f"An error occurred:\n{e}")
+
+
 
 class Mainwindow(QtWidgets.QMainWindow):
     def __init__(self):
