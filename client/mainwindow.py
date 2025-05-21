7a650435237a5f340bdbd60c7679b5dd48d4d319
victoria.schwabe <victoria.schwabe@stud.th-luebeck.de>
2025-05-21 11:49:17 +0200

    fix: merged login and register

--- client/mainwindow.py
+++ client/mainwindow.py
@@ -1,19 +1,64 @@
 import sys
+import unittest
 import sqlite3
 from PyQt6 import QtWidgets, uic
+from client import *
+from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
-
 
 class Registerwindow(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi("register.ui", self)
 
+        self.pushButton_register.clicked.connect(self.register)
+
+    def register(self):
+
+        username = self.lineEdit_username.text()
+        password = self.lineEdit_password.text()
+        email = self.lineEdit_email.text()
+
+        hashed_password = generate_password_hash(password)
+
+        if username == '' or password == '' or email == '':
+            QtWidgets.QMessageBox.warning(self, "Error", "Please fill all fields")
+            return
+        if '@' not in email or '.' not in email:
+            QtWidgets.QMessageBox.warning(self, "Error", "Enter a valid email address.")
+            return
+
+        try:
+            print('connecting ...')
+            conn = sqlite3.connect('crypto.db')
+            c = conn.cursor()
+            print('Executing Insert')
+
+            c.execute('''
+                    INSERT INTO user (username, password, email) 
+                    VALUES (?,?,?)
+                ''', (username, hashed_password, email))
+            conn.commit()
+            print('Insert done')
+            QtWidgets.QMessageBox.information(self, 'Account created',
+                                              "Your account has been created! You are now able to log in.")
+            self.close()
+
+        except sqlite3.Error as e:
+            print(f"SQLite Error: {e}")
+            QtWidgets.QMessageBox.warning(self, "Error", f"SQLite Error: {e}")
+
+        finally:
+            conn.close()
+
 
 class Loginwindow(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi("login.ui", self)
+
+        self.Register.clicked.connect(self.show_login)
+        self.register_window = None
 
         self.pushButton_to_login.clicked.connect(self.verify_login)
 
@@ -36,6 +81,11 @@ class Loginwindow(QtWidgets.QDialog):
                 QtWidgets.QMessageBox.warning(self, "Login Failed", "Invalid Username or Password")
         except Exception as e:
             QtWidgets.QMessageBox.critical(self, "Error", f"An error occurred:\n{e}")
+
+    def show_login(self):
+        self.register_window = Registerwindow()
+        self.register_window.show()
+
 
 
 
