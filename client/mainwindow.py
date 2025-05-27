7de7f0be760ca0dcfa2bc8bfbd02d0a74e9021f8
Schwabe, Victoria <victoria.schwabe@stud.th-luebeck.de>
2025-05-27 13:55:07 +0000

    feat: integratin test fix:mainwindow

--- client/mainwindow.py
+++ client/mainwindow.py
@@ -1,17 +1,18 @@
 import sys
 import json
 import sqlite3
-
 from PyQt6 import QtWidgets, uic
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import os
 
 
 class Registerwindow(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi("register.ui", self)
+        ui_path = os.path.join(os.path.dirname(__file__), "register.ui")
+        uic.loadUi(ui_path, self)
 
         self.pushButton_register.clicked.connect(self.register)
 
@@ -50,6 +51,7 @@ class Registerwindow(QtWidgets.QDialog):
                 "Your account has been created! You are now able to log in.",
             )
             self.close()
+            return "success"
 
         except sqlite3.Error as e:
             print(f"SQLite Error: {e}")
@@ -57,6 +59,24 @@ class Registerwindow(QtWidgets.QDialog):
 
         finally:
             conn.close()
+
+
+    def register(self):
+        username = self.lineEdit_username.text()
+        password = self.lineEdit_password.text()
+        email = self.lineEdit_email.text()
+
+        result = self.create_new_user(username, password, email)
+
+        if result == "empty":
+            QtWidgets.QMessageBox.warning(self, "Error", "Please fill all fields")
+        elif result == "notValid":(
+            QtWidgets.QMessageBox.warning(self, "Error", "Enter a valid email address."))
+        elif result == "success":
+            QtWidgets.QMessageBox.information(self, 'Account created',
+                                              "Your account has been created! You are now able to log in.")
+            self.close()
+
 
 
 class Loginwindow(QtWidgets.QDialog):
@@ -103,12 +123,13 @@ class Mainwindow(QtWidgets.QMainWindow):
         uic.loadUi("form.ui", self)
 
         self.loginButton.clicked.connect(self.login_show)
-        self.Accounts_2.itemClicked.connect(self.fetch_table)
+#        self.Accounts_2.itemClicked.connect(self.fetch_table())
         self.login_window = None
 
     def login_show(self):
         self.login_window = Loginwindow()
         self.login_window.show()
+
 
     def fetch_table(self, item):
         coinName = item.text()
