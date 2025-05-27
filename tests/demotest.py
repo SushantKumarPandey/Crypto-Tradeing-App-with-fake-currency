3474ed2ff784c23322c4a0592233de0aec8e883d
Schwabe, Victoria <victoria.schwabe@stud.th-luebeck.de>
2025-05-27 13:55:07 +0000

    feat: integratin test fix:mainwindow

--- tests/demotest.py
+++ tests/demotest.py
@@ -1,29 +1,55 @@
 import sqlite3
 import unittest
+from client.mainwindow import Registerwindow
+from werkzeug.security import check_password_hash
+from PyQt6.QtWidgets import QApplication
+import sys
 
+app = QApplication(sys.argv)  # Einmalig vor allen Tests, z.B. ganz oben in der Datei
+
+
+testDB = "../client/crypto.db"
 
 class TestDB(unittest.TestCase):
 
+    def setUp(self):
+        self.conn = sqlite3.connect(testDB)
+        self.c = self.conn.cursor()
+
+        self.c.execute('''
+         CREATE TABLE IF NOT EXISTS user(
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         username TEXT UNIQUE NOT NULL,
+         password TEXT NOT NULL,
+         email TEXT NOT NULL
+         )
+     ''')
+        self.c.execute('DELETE FROM user WHERE username = ?', ("tester",))
+        self.c.execute('INSERT INTO user (username, password, email) VALUES (?, ?, ?)',
+                  ("tester", "password", "bsp@email.com"))
+
+        self.conn.commit()
+        self.conn.close()
+
     def test_connected(self):
         try:
-            conn = sqlite3.connect('../client/crypto.db')
+            conn = sqlite3.connect(testDB)
             c = conn.cursor()
-            state="Conected"
+            state = "Conected"
             conn.close()
         except:
-            state="Disconnected"
+            state = "Disconnected"
         self.assertEqual(state, "Conected")
 
-
-    def test_getAllUsers(self):
-        conn = sqlite3.connect('../client/crypto.db')
+    def test_getUsers(self):
+        conn = sqlite3.connect(testDB)
         c = conn.cursor()
-        c.execute("SELECT username FROM user WHERE id < 3")
-        users = [row[0] for row in c.fetchall()]
-        expectedNames = ["kiki", "marcel"]
-        self.assertEqual(users, expectedNames)
 
+        c.execute("SELECT username FROM user WHERE username =  ?", ("tester",))
+        user =c.fetchone()
         conn.close()
+        assert(user is not None)
+        assert(user[0] == "tester")
 
     def test_password_fromUser(self):
         conn = sqlite3.connect('../client/crypto.db')
@@ -34,14 +60,86 @@ class TestDB(unittest.TestCase):
 
         conn.close()
 
+    def test_hasedPassword(self):
+        conn = sqlite3.connect('../client/crypto.db')
+        c = conn.cursor()
+        c.execute("SELECT password FROM user WHERE username = 'safe' ")
+        hashed_password = c.fetchone()[0]
+        password = "safe"
+
+        self.assertNotEqual(hashed_password, 'safe')
+        self.assertTrue(check_password_hash(hashed_password, password))
+        conn.close()
+
     def test_email_fromUser(self):
         conn = sqlite3.connect('../client/crypto.db')
         c = conn.cursor()
         c.execute("SELECT email FROM user WHERE username = 'kiki' ")
-        password = c.fetchone()[0]
-        self.assertEqual(password, 'bsp1@gmail.com')
+        email = c.fetchone()[0]
+        self.assertEqual(email, 'bsp1@gmail.com')
 
         conn.close()
+
+class TestHTTP(unittest.TestCase):
+
+    def setUp(self):
+        self.conn = sqlite3.connect(testDB)
+        self.c = self.conn.cursor()
+
+        self.c.execute('''
+            CREATE TABLE IF NOT EXISTS user(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            email TEXT NOT NULL
+            )
+        ''')
+        self.c.execute('DELETE FROM user WHERE username = ?', ("tee",))
+        self.c.execute('DELETE FROM user WHERE username = ?', ("user",))
+        self.conn.commit()
+        self.conn.close()
+        self.window=Registerwindow()
+
+    def test_createUser(self):
+        new_user = self.window.create_new_user("tee", "secure", "email@example.com")
+        self.assertEqual(new_user, "success")
+
+    def test_usernameUnique(self):
+        conn = sqlite3.connect(testDB)
+        c = conn.cursor()
+        try:
+            self.c.execute('INSERT INTO user (username, password, email) VALUES (?, ?, ?)',
+                  ("tester", "password", "bsp@email.com"))
+            message="new accouont"
+        except:
+            message="name already taken"
+        conn.close()
+        self.assertEqual(message, "name already taken")
+
+    def test_email(self):
+        result = self.window.create_new_user('user', 'pass', 'no-at-sign')
+        self.assertEqual(result, 'notValid')
+
+
+# class TestAPI(unittest.TestCase):
+'''
+!API-TEST!
+    def test_get_session(self):
+        url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map"
+        headers = {
+            'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY': '8bc7959e-153c-40dd-8da9-34e544661e71'
+        }
+
+        session = requests.Session()
+        session.headers.update(headers)
+
+
+        response = session.get(url)
+        assert response.status_code == 200, "error not cnected"
+        assert response.text == "OK"
+'''
+
 
 
 if __name__ == '__main__':
