7e2a932f43b292811a2748a7d08faeb57b8e7194
victo <victoria.S03@outlook.como>
2025-06-03 13:34:49 +0200

    feat: tutorial und guide funktion mit infos

--- server/user.sql
+++ server/user.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS coin;
 DROP TABLE IF EXISTS holding;
+DROP TABLE IF EXISTS guides;
+DROP TABLE IF EXISTS tutorial;
+
 
 CREATE TABLE user(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -8,6 +11,20 @@ CREATE TABLE user(
     password TEXT NOT NULL,
     email TEXT NOT NULL
 );
+
+
+CREATE TABLE guides(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nameG TEXT UNIQUE NOT NULL,
+    info TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE tutorial(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nameT TEXT UNIQUE NOT NULL,
+    info TEXT UNIQUE NOT NULL
+);
+
 
 CREATE TABLE coin(
     id INTEGER PRIMARY KEY,
@@ -32,3 +49,56 @@ INSERT INTO user (username, password, email)
 
 INSERT INTO holding (user_id, coin_symbol, amount)
     VALUES (2, 'BTC', 3);
+
+
+INSERT INTO guides (nameG, info)
+    VALUES ('GA', 'ALorem ipsum dolor sit amet, ' ||
+                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
+                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
+                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
+                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
+                  'Lorem ipsum dolor sit amet'),
+('GB', 'BLorem ipsum dolor sit amet, ' ||
+                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
+                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
+                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
+                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
+                  'Lorem ipsum dolor sit amet'),
+('GC', 'CLorem ipsum dolor sit amet, ' ||
+                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
+                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
+                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
+                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
+                  'Lorem ipsum dolor sit amet');
+
+INSERT INTO tutorial (nameT, info)
+    VALUES ('TA', 'ALorem ipsum dolor sit amet, ' ||
+                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
+                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
+                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
+                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
+                  'Lorem ipsum dolor sit amet'),
+('TB', 'BLorem ipsum dolor sit amet, ' ||
+                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
+                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
+                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
+                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
+                  'Lorem ipsum dolor sit amet'),
+('TC', 'CLorem ipsum dolor sit amet, ' ||
+                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
+                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
+                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
+                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
+                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
+                  'Lorem ipsum dolor sit amet');
