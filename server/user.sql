a655d19554f092e3625af51dbdc163a4359f1e0c
victo <victoria.S03@outlook.como>
2025-06-15 21:23:44 +0200

    fix: inhalt für tutorial

--- server/user.sql
+++ server/user.sql
@@ -52,53 +52,18 @@ INSERT INTO holding (user_id, coin_symbol, amount)
 
 
 INSERT INTO guides (nameG, info)
-    VALUES ('GA', 'ALorem ipsum dolor sit amet, ' ||
-                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
-                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
-                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
-                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
-                  'Lorem ipsum dolor sit amet'),
-('GB', 'BLorem ipsum dolor sit amet, ' ||
-                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
-                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
-                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
-                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
-                  'Lorem ipsum dolor sit amet'),
-('GC', 'CLorem ipsum dolor sit amet, ' ||
-                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
-                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
-                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
-                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
-                  'Lorem ipsum dolor sit amet');
+    VALUES ('GA', 'ALorem ipsum dolor sit amet, '),
+('GB', 'BLorem ipsum dolor sit amet, '),
+('GC', 'CLorem ipsum dolor sit amet, ');
 
 INSERT INTO tutorial (nameT, info)
-    VALUES ('TA', 'ALorem ipsum dolor sit amet, ' ||
-                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
-                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
-                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
-                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
-                  'Lorem ipsum dolor sit amet'),
-('TB', 'BLorem ipsum dolor sit amet, ' ||
-                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
-                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
-                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
-                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
-                  'Lorem ipsum dolor sit amet'),
-('TC', 'CLorem ipsum dolor sit amet, ' ||
-                  'consetetur sadipscing elitr, sed diam nonumy eirmod ' ||
-                  'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' ||
-                  'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' ||
-                  'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' ||
-                  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' ||
-                  'Lorem ipsum dolor sit amet');
+    VALUES ('What is Bitcoin?', 'It ia a cryptocurrency, which was launched in 2009. It is Interessting for Investors, because the price is very volatile.'),
+
+('Where to start?', 'The best start would be, to look at the top list and check the data of the current market.'),
+
+('Your first invest:', 'Befor your first invest take into account, that the amount of the value will be transformed into the coin value. \n When you want to sell, make sure to sell to a higher value'),
+
+('where to follow your invests?', 'In your Portfolio you can monitor you invests, with the current value and if yoou would make plus or minus'),
+
+('Now Try yourself!', 'for completing this boring tutorial you get 10.000 coins! Good Luck!');
+
